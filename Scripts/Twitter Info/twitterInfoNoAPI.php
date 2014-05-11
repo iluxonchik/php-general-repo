ba6d4d44220d7742934c902a8a1f7296776396b1
iluxonchik <iluxon4ik@hotmail.com>
2014-05-11 02:43:30 +0100

    getName() updated | getAbout() implemented.
    getName() now works for verified and non verified users. getAbout()
    implemented, this one as well works for verified and non verified users.

--- Scripts/Twitter Info/twitterInfoNoAPI.php
+++ Scripts/Twitter Info/twitterInfoNoAPI.php
@@ -9,6 +9,12 @@
 define('TWITTER_URL', 'https://mobile.twitter.com/');
 define('TWITTER_200', 'HTTP/1.0 200 OK'); // OK response header
 define('MAX_STRING_LENGTH', 200);
+define('VERIFIED_USER_REGEX', "/<div class=\"fullname\">(.*?)<a/");
+define('NON_VERIFIED_USER_REGEX', "/<div class=\"fullname\">(.*?)<\/div>/");
+define('USER_POSITION', "<div class=\"fullname\">");
+define('ABOUT_POSITION', "<div class=\"dir-ltr\" dir=\"ltr\">");
+define('ABOUT_REGEX', "/<div class=\"dir-ltr\" dir=\"ltr\">(.*?)<\/div>/");
+define('ERROR_INVALID_USER', -1);
 
 class TwitterInfo {
 
@@ -19,6 +25,7 @@ class TwitterInfo {
 		if(isset($user) && !empty($user)){
 			$this->user = $user;
 			$this->mobileURL = TWITTER_URL . $this->user; // generate user URL
+			$this->urlCont = file_get_contents($this->mobileURL);
 		}
 		else
 			$this->validUser = false; // the argument is empty or nonexistent
@@ -32,19 +39,34 @@ class TwitterInfo {
 	}
 
 	function getName(){
-		// get twitter name from twitter url
-		// TODO: this only works for non-verified profiles
-		$urlCont = file_get_contents($this->mobileURL);
-		$position = strpos($urlCont, "<div class=\"fullname\">");
-		$subStr = substr($urlCont, $position, MAX_STRING_LENGTH);
-		$subStr = preg_replace( "/\r|\n/", "", $subStr); // remove new line chars (for preg_match)
-		preg_match("/<div class=\"fullname\">(.*?)<\/div>/", $subStr , $match);
-		$this->name = trim($match[1]); // remove spaces
-		return $this->name;
+		if ($this->validUser){
+			// get twitter name from twitter url
+			$position = strpos($this->urlCont, USER_POSITION);
+			$subStr = substr($this->urlCont, $position, MAX_STRING_LENGTH);
+			$subStr = preg_replace( "/\r|\n/", "", $subStr); // remove new line chars (for preg_match)
+			preg_match(NON_VERIFIED_USER_REGEX, $subStr , $match);
+			if (empty($match)){
+				// if the $match is empty, try another regex
+				// this is a regular expression in case the user is verified
+				preg_match(VERIFIED_USER_REGEX, $subStr , $match);
+			}
+			$this->name = trim($match[1]); // remove spaces
+			return $this->name;
+		}
+		else
+			return ERROR_INVALID_USER; // the user is not valud
 	}
 
 	function getAbout(){
-		// TODO
-		return 0;
+		if($this->validUser){
+			$position = strpos($this->urlCont, ABOUT_POSITION);
+			$subStr = substr($this->urlCont, $position, MAX_STRING_LENGTH); // get just a part of the urls HTML
+			$subStr = preg_replace( "/\r|\n/", "", $subStr); // remove new line chars (for preg_match)
+			preg_match(ABOUT_REGEX, $subStr , $match);
+			$this->about = trim($match[1]); // remove spaces from the end and the beginning
+			return $this->about;
+		}
+		else
+			return ERROR_INVALID_USER; // the user is not valid
 	}
 }
